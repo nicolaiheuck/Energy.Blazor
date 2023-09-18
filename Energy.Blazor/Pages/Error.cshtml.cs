33e01534e62b3a8d0e70a825c85c8790ecd2f75b
Peter Hymøller <peter.hymoeller@hotmail.dk>
2023-09-18 09:29:31 +0200

    updated frontend

--- Energy.Blazor/Pages/Error.cshtml.cs
+++ Energy.Blazor/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Energy.Blazor.Pages;
+namespace Canteen.Blazor.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
