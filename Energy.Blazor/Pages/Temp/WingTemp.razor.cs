3e082e51ff87c62afa760f556af579824e99ceb3
Peter Hymøller <peter.hymoeller@hotmail.dk>
2023-09-22 11:08:08 +0200

    updated tempreture

---
+++ Energy.Blazor/Pages/Temp/WingTemp.razor.cs
@@ -0,0 +1,49 @@
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Toolbelt.Blazor.HotKeys2;
+
+namespace Energy.Blazor.Pages.Temp
+{
+    public partial class WingTemp : IDisposable
+    {
+        [Inject]
+        public IToastService? ToastService { get; set; }
+
+        [Inject]
+        public HotKeys? HotKeys { get; set; }
+
+        [Inject]
+        public Toolbelt.Blazor.I18nText.I18nText? I18nText { get; set; }
+
+        //public ? SelectedDetailedWing { get; set; }
+        //public ? SelectedDetailedroom { get; set; }
+        public const int PageSize = 5;
+        public int Count;
+
+        //private RadzenDataGrid<Wing>? _wingInformationGrid;
+        //private List<Wings> _wingInformationVms = new();
+
+        private HotKeysContext? _hotKeysContext;
+        private I18nText.LanguageTable _languageTable = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            ArgumentNullException.ThrowIfNull(HotKeys);
+            ArgumentNullException.ThrowIfNull(I18nText);
+
+            _languageTable = await I18nText.GetTextTableAsync<I18nText.LanguageTable>(this);
+            _hotKeysContext = HotKeys.CreateContext()
+                .Add(Code.F8, Toaster);
+        }
+        void Toaster()
+        {
+            ArgumentNullException.ThrowIfNull(ToastService);
+            ToastService.ShowInfo("Congratulations, you just pressed hotkey: F8");
+        }
+
+        public void Dispose()
+        {
+            _hotKeysContext?.Dispose();
+        }
+    }
+}
