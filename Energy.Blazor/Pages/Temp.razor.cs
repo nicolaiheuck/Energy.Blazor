beb2c7332111f6dd9951b72218e80488cc49ef50
Peter Hymøller <peter.hymoeller@hotmail.dk>
2023-09-22 10:42:12 +0200

    added pages

---
+++ Energy.Blazor/Pages/Temp.razor.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Toolbelt.Blazor.HotKeys2;
+
+namespace Energy.Blazor.Pages
+{
+	public partial class Temp : IDisposable
+	{
+		[Inject]
+		public IToastService? ToastService { get; set; }
+
+		[Inject]
+		public HotKeys? HotKeys { get; set; }
+
+		[Inject]
+		public Toolbelt.Blazor.I18nText.I18nText? I18nText { get; set; }
+
+		private HotKeysContext? _hotKeysContext;
+		private I18nText.LanguageTable _languageTable = new();
+
+		protected override async Task OnInitializedAsync()
+		{
+			ArgumentNullException.ThrowIfNull(HotKeys);
+			ArgumentNullException.ThrowIfNull(I18nText);
+
+			_languageTable = await I18nText.GetTextTableAsync<I18nText.LanguageTable>(this);
+			_hotKeysContext = HotKeys.CreateContext()
+				.Add(Code.F8, Toaster);
+		}
+		void Toaster()
+		{
+			ArgumentNullException.ThrowIfNull(ToastService);
+			ToastService.ShowInfo("Congratulations, you just pressed hotkey: F8");
+		}
+
+		public void Dispose()
+		{
+			_hotKeysContext?.Dispose();
+		}
+	}
+}
